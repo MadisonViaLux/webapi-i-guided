abcb9b0fd92e72ef565e2956dd3e394e7ba3acd7
MadisonViaLux <madison.s.mckown@gmail.com>
2019-08-05 13:02:57 -0700

    done

--- index.js
+++ index.js
@@ -6,11 +6,41 @@ const Hubs = require('./data/hubs-model')
 
 const server = express();
 
+server.use(express.json())
+
+
+
+
+
 
 
 server.get('/', (req, res) => {
     res.send('hello web20.75');
 })
+
+
+
+
+
+
+server.post('/hubs', (req, res) => {
+
+    const hubInfo = req.body;
+
+    console.log('hub info from body', hubInfo)
+
+    Hubs.add(hubInfo)
+    .then(hub => {
+        res.status(201).json(hub)
+    })
+    .catch(error => {
+        res.status(500).json({ message: 'error adding the hub' })
+    })
+})
+
+
+
+
 
 server.get('/hubs', (req, res) => {
     Hubs.find().then(hubs => {
@@ -19,6 +49,45 @@ server.get('/hubs', (req, res) => {
         res.status(500).json({ message: 'error getting list of hubs' })
     })
 })
+
+
+
+
+
+server.delete('/hubs/:id', (req, res) => {
+    const hubId = req.params.id;
+
+    Hubs.remove(hubId)
+        .then(hub => {
+            res.status(201).json({message: 'hub deleted'})
+        })
+        .catch(error => {
+            res.status(500).json({ message: 'error deleting the hub' })
+        })
+})
+
+
+server.put('/hubs/:id', (req, res) => {
+    const { id } = req.params;
+    const changes = req.body;
+
+    Hubs.update(id, changes)
+        .then(updated => {
+            if(updated){
+                res.status(200).json(updated);
+            } else {
+                res.status(404).json({ message: 'hub not found' })
+            }
+        })
+        .catch(error => {
+            res.status(500).json({ message: 'error updating hub' })
+        })
+})
+
+
+
+
+
 
 
 
